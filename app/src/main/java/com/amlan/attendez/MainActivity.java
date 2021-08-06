d1f8bfb43bff69741a64b7a253a4e9c260ac7d7f
ImAmlan03 <imamlan03@gmail.com>
2021-08-06 22:22:11 +0530

    Added Firebase Authentication

--- app/src/main/java/com/amlan/attendez/MainActivity.java
+++ app/src/main/java/com/amlan/attendez/MainActivity.java
@@ -1,5 +1,6 @@
 package com.amlan.attendez;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,10 +8,22 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etEmail, etPword;
+    FloatingActionButton fabLogin;
+    FirebaseAuth mAuth;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -20,9 +33,56 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_main);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPword = findViewById(R.id.etPword);
+        fabLogin = findViewById(R.id.fabLogin);
+        mAuth = FirebaseAuth.getInstance();
+
+        fabLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = etEmail.getText().toString().trim();
+                String password = etPword.getText().toString().trim();
+
+                if (email.isEmpty()) {
+                    etEmail.setError("Please enter an email");
+                    etEmail.requestFocus();
+                    return;
+                }
+                if (password.isEmpty()) {
+                    etPword.setError("Please set a passsword");
+                    etPword.requestFocus();
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    etEmail.setError("email address not valid");
+                    etEmail.requestFocus();
+                    return;
+                }
+                if (password.length() < 6) {
+                    etPword.setError("Provide atleast 6 characters");
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                    Intent i = new Intent(MainActivity.this, ClassActivity.class);
+                                    startActivity(i);
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 
     public void OpenSignupPage(View view) {
-        startActivity(new Intent(MainActivity.this,SignupActivity.class));
+        startActivity(new Intent(MainActivity.this, SignupActivity.class));
     }
 }
--- app/src/main/java/com/amlan/attendez/SignupActivity.java
+++ app/src/main/java/com/amlan/attendez/SignupActivity.java
@@ -1,14 +1,36 @@
 package com.amlan.attendez;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private EditText etName, etEmail, etPword;
+    private RadioGroup RGroup;
+    private RadioButton radioButton;
+    private FloatingActionButton fabProceed;
+    private TextView tvSignin;
+    private FirebaseAuth mAuth;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -18,5 +40,84 @@ public class SignupActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_signup);
+
+        etName = findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
+        etPword = findViewById(R.id.etPword);
+        RGroup = findViewById(R.id.RGroup);
+        fabProceed = findViewById(R.id.fabProceed);
+        tvSignin = findViewById(R.id.tvSignin);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
+            Intent i = new Intent(SignupActivity.this, ClassActivity.class);
+            startActivity(i);
+            finish();
+        }
+
+        tvSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        fabProceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int radioId = RGroup.getCheckedRadioButtonId();
+                radioButton = findViewById(radioId);
+                String name = etName.getText().toString().trim();
+                String email = etEmail.getText().toString().trim();
+                String password = etPword.getText().toString().trim();
+                String userType = radioButton.getText().toString().trim();
+
+                if (name.isEmpty()) {
+                    etName.setError("Please enter your Name");
+                    etName.requestFocus();
+                    return;
+                }
+                if (email.isEmpty()) {
+                    etEmail.setError("Please enter an email");
+                    etEmail.requestFocus();
+                    return;
+                }
+                if (password.isEmpty()) {
+                    etPword.setError("Please set a passsword");
+                    etPword.requestFocus();
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    etEmail.setError("email address not valid");
+                    etEmail.requestFocus();
+                    return;
+                }
+                if (password.length() < 6) {
+                    etPword.setError("Provide atleast 6 characters");
+                    return;
+                }
+                if (userType.matches("")) {
+                    Toast.makeText(SignupActivity.this, "Select user type", Toast.LENGTH_SHORT).show();
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignupActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(SignupActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+
+    public void checkButton(View v) {
+        int radioId = RGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(radioId);
     }
 }
